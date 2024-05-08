57240574c0989565ee3f27499ff2aa108cd0ea66
Chanel Morgan <chanelmorgan05@icloud.com>
2024-05-08 12:59:11 +0100

    Basic structure complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,38 @@
+import { CustomerReviews, Footer, Hero, PopularProducts, Services, SpecialOffer, Subscribe, SuperQuality} from './sections';
+
 const App = () => ( 
   <main className='relative'>
-    Nav 
+    <Nav /> 
     <section className="xl:padding-1 wide:padding-r padding-b">
-      Hero
+      <Hero />
     </section>
 
     <section className="padding"> 
-    Popular products 
+    <PopularProducts /> 
     </section>
 
     <section className="padding"> 
-    Super quality   
+    <SuperQuality  /> 
     </section>
 
     <section className="padding-x py-10"> 
-    Services 
+    <Services /> 
     </section>
 
     <section className="padding"> 
-    Special offers 
+    <SpecialOffer /> 
     </section>
 
     <section className="bg-pale-blue padding"> 
-    Customer reviews 
+    <CustomerReviews /> 
     </section>
 
     <section className="padding-x sm:py-32 py-16 w-full"> 
-    Subscribe 
+    <Subscribe /> 
     </section>
 
     <section className="bg-black padding-x padding-t pb-8"> 
-    Footer
+    <Footer />
     </section>
 
   </main>
