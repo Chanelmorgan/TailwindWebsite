d0387a4bcee65f8a73e9170f1f11700bfb8717de
Chanel Morgan <chanelmorgan05@icloud.com>
2024-05-08 12:41:36 +0100

    updating the main components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,39 @@
-export default function App() {
+const App = () => ( 
+  <main className='relative'>
+    Nav 
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      Hero
+    </section>
 
-  return (
-    <h1 className="text-3xl font-bold underline"> 
-        Hello World 
-    </h1>
-  )
-}
+    <section className="padding"> 
+    Popular products 
+    </section>
+
+    <section className="padding"> 
+    Super quality   
+    </section>
+
+    <section className="padding-x py-10"> 
+    Services 
+    </section>
+
+    <section className="padding"> 
+    Special offers 
+    </section>
+
+    <section className="bg-pale-blue padding"> 
+    Customer reviews 
+    </section>
+
+    <section className="padding-x sm:py-32 py-16 w-full"> 
+    Subscribe 
+    </section>
+
+    <section className="bg-black padding-x padding-t pb-8"> 
+    Footer
+    </section>
+
+  </main>
+); 
+export default App; 
 
